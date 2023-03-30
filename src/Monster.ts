dab5d265d0f561fa66af8e09842375515a187fea
Victor Hugo <victorhdoliveira@gmail.com>
2023-03-30 17:09:11 -0300

    Feat: Classe Monster

---
+++ src/Monster.ts
@@ -0,0 +1,33 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;   
+  
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = this.lifePoints - attackPoints;
+    this._lifePoints -= damage;
+
+    const lifePoints = (this._lifePoints <= 0)
+      ? this._lifePoints = -1 : this._lifePoints;
+
+    return lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
